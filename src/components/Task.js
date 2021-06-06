7d2ace33ada0fa833a9129dc8b97b3e446885254
Joshua Mondoy <joshuamondoy24@gmail.com>
2021-06-06 21:14:51 +0800

    try

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import { FaTimes } from 'react-icons/fa' //this is for the icon we download usin
 const Task = ({task, onDelete}) => {
     return (
         <div className='task'>
-            <h3>{task.text} <FaTimes className='x-icon' onClick={() => onDelete(task.id)}/></h3> {/*the onclick function will get the id that will be click to delete*/}
+            <h3>{task.text} <FaTimes className='x-icon' title={'Delete'} onClick={() => onDelete(task.id)}/></h3> {/*the onclick function will get the id that will be click to delete*/}
             
             <p>{task.day}</p>
         </div>
